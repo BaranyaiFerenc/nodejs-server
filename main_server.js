5a837af16cb474fe281f27082f08e6a6bc763dd8
BaranyaiFerenc <114213495+BaranyaiFerenc@users.noreply.github.com>
2024-08-16 08:55:43 +0000

    Working server

--- main_server.js
+++ main_server.js
@@ -1,1 +1,18 @@
 
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    fs.readFile('html/demopage.html', function(err, data) {
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end('<html><body style="background-color: rgb(173, 83, 83);"><h1 style="color: white; text-align: center; margin-top: 25%; font-size: xx-large;">ERROR 404</h1><h3 style="color: white; text-align: center;">The searched page not found!</h3></body></html>');
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+
+        return res.end();
+    });
+    
+}).listen(8080);
