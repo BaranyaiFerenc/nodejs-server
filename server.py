73f398a58abca572794ee9cdda7b527d9ec96e8e
BaranyaiFerenc <baranyai.fer@gmail.com>
2024-08-19 23:59:50 +0200

    Added pages and searchbar

--- server.py
+++ server.py
@@ -5,10 +5,31 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def error(errorCode, errorMsg):
+    return render_template('/errorpage.html', code=errorCode, message=errorMsg)
+
+
 @app.route('/')
 @app.route('/<name>')
-def hello(name=None):
-    return render_template('/demopage.html', person=name)
+def hello(name="Anonymus"):
+    return render_template('/demopage.html', user=name)
+
+
+@app.route('/user/<username>')
+def user_page(username=None):
+
+    if True:
+        return render_template('/user.html', user=username)
+    else:
+        return error(404, "User not found!")
+    
+@app.route('/search/<search>')
+def search(search=None):
+
+    if search != None:
+        return error(200, "Searched: "+search)
+    else:
+        return error(404, "Searched item not found!")
 
 
 if __name__ == '__main__':
